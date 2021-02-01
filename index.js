8c2c6a961e3deba65b7356342795e44946f77de9
Alvaro Dalmau <dalmau.alvaro@gmail.com>
2021-02-01 17:55:34 +0100

    done

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
+const usersArray = [
+  {
+    firstName: 'Kirby',
+    lastName: 'Doyle',
+    id: 'b71794e5-851e-44b5-9eec-1dd4e897e3b8',
+    isActive: false,
+    balance: '$3,570.06',
+    gender: 'male',
+  },
+  {
+    firstName: 'Tracie',
+    lastName: 'May',
+    id: '1af0e9ee-66fc-4298-b8ce-5d99bcbaac05',
+    isActive: false,
+    balance: '$1,547.73',
+    gender: 'female',
+  },
+  {
+    firstName: 'Kendra',
+    lastName: 'Hines',
+    id: '5e92af3a-b08e-4689-bdeb-3226300470e3',
+    isActive: true,
+    balance: '$12,383.08',
+    gender: 'female',
+  },
+  {
+    firstName: 'Kinney',
+    lastName: 'Howard',
+    id: '0ad2388d-83e1-4831-9cc4-e3581f8edf36',
+    isActive: false,
+    balance: '$3,207.06',
+    gender: 'male',
+  },
+  {
+    firstName: 'Howard',
+    lastName: 'Gilmore',
+    id: '0719205d-c965-44cb-a128-708cf335b26c',
+    isActive: true,
+    balance: '$21,307.75',
+    gender: 'male',
+  },
+  {
+    firstName: 'Rachelle',
+    lastName: 'Schneider',
+    id: '04012184-651b-41eb-9642-d362fedff02f',
+    isActive: true,
+    balance: '$35,121.49',
+    gender: 'female',
+  },
+  {
+    firstName: 'Lizzie',
+    lastName: 'Alford',
+    id: '598ca7ec-888e-494d-ae94-c21ace3ffa52',
+    isActive: false,
+    balance: '$4,382.94',
+    gender: 'female',
+  },
+];
+
 // ***************************************************************************
 // Iteration 1 - `for...of` loop
 // ***************************************************************************
@@ -5,8 +64,9 @@
 const getFirstNames = arr => {
   const userFirstNames = [];
   for (let user of arr) {
-    // Your code goes here ...
+    userFirstNames.push(user.firstName);
   }
+  return userFirstNames;
 };
 
 getFirstNames(usersArray);
@@ -19,6 +79,11 @@ getFirstNames(usersArray);
 
 const getFullNames = arr => {
   // Your code goes here ...
+  const userFullNames = [];
+  for (let value of arr) {
+    userFullNames.push(`${value.firstName} ${value.lastName}`);
+  }
+  return userFullNames;
 };
 
 getFullNames(usersArray);
@@ -32,6 +97,19 @@ getFullNames(usersArray);
 
 const getUsersCreditDetails = arr => {
   // Your code goes here ...
+  usersCreditDetails = [];
+  for (let value of arr) {
+    let { firstName } = value; // firstName=value.firstName
+    let { lastName } = value;
+    let { balance } = value;
+    const userDetails = {
+      firstName,
+      lastName,
+      balance,
+    };
+    usersCreditDetails.push(userDetails);
+  }
+  return usersCreditDetails;
 };
 
 getUsersCreditDetails(usersArray);
@@ -50,6 +128,15 @@ getUsersCreditDetails(usersArray);
 
 const genderView = users => {
   // Your code goes here ...
+  let femaleUsers = users.filter(value => {
+    return value.gender == 'female';
+  });
+  let maleUsers = users.filter(value => {
+    return value.gender == 'male';
+  });
+  femaleUsers = getFullNames(femaleUsers);
+  maleUsers = getFullNames(maleUsers);
+  return { femaleUsers, maleUsers };
 };
 
 genderView(usersArray);
